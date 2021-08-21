9cf6e125aec5d3a6053139d88acb4e8bb85496a3
Aslamov Yury <aslamov.yura@gmail.com>
2021-08-21 22:39:41 +0300

    Added base Application interfaces and Domain Events service.

---
+++ src/Application/Common/Interfaces/IDateTime.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Application.Common.Interfaces
+{
+    /// <summary>
+    /// Interface for entities, containing Date/Time.
+    /// </summary>
+    public interface IDateTime
+    {
+        /// <summary>
+        /// Current Data and Time.
+        /// </summary>
+        DateTime Now { get; set; }
+    }
+}
---
+++ src/Infrastructure/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using Application.Common.Interfaces;
+using Infrastructure.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            // Add DB Context.
+
+            // Add Services.
+            services.AddScoped<IDomainEventService, DomainEventService>();
+
+            return services;
+        }
+    }
+}
