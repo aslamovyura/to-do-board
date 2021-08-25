8c4153a0afc241a2032a87e05c60d1460f1cba09
Aslamov Yury <aslamov.yura@gmail.com>
2021-08-25 22:42:53 +0300

    Configure application startup

---
+++ src/WebUI/Extensions/HostExtensions.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Threading.Tasks;
+using Infrastructure.Identity;
+using Infrastructure.Persistence;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace WebUI.Extensions
+{
+    /// <summary>
+    /// Define extensions for <see cref="IHost"/> entities.
+    /// </summary>
+    public static class HostExtensions
+    {
+        /// <summary>
+        /// Seed database with sample data.
+        /// </summary>
+        /// <param name="host">Host builder.</param>
+        public static async Task SeedDatabaseAsync(this IHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    var context = services.GetRequiredService<ApplicationDbContext>();
+
+                    if (context.Database.IsSqlServer())
+                    {
+                        context.Database.Migrate();
+                    }
+
+                    var userManager = services.GetRequiredService<UserManager<ApplicationUser>>();
+                    var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+
+                    await ApplicationDbContextSeed.SeedDefaultUserAsync(userManager, roleManager);
+                    await ApplicationDbContextSeed.SeedSampleDataAsync(context);
+                }
+                catch (Exception ex)
+                {
+                    var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+
+                    logger.LogError(ex, "An error occurred while migrating or seeding the database.");
+
+                    throw;
+                }
+            }
+        }
+    }
+}
